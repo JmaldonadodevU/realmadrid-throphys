8fdc3b83ccd50ba8f1721dfc6ba3822226b3b6d8
JULIAN LEONARDO MALDONADO PEREZ <julian.maldonadop@uniagustiniana.edu.co>
2025-09-05 19:18:31 -0500

    Feature: Integración del componente TrophyBoard en la carpeta maldonado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Prueba from './secciones/prueba'
 import './App.css'
+import TrophyBoard from './secciones/maldonado'
 
 function App() {
   return (
     <>
       <Prueba />
+      <TrophyBoard />
     </>
   )
 }
